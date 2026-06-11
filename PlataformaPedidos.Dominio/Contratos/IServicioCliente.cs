5af4a760460f4645b1933e23a6a19e2d58ce5356
Matias Silioni <msilioni@gmail.com>
2026-06-11 19:31:57 -0300

    agregue la implementacion de los servicios pendientes

---
+++ PlataformaPedidos.Dominio/Contratos/IServicioCliente.cs
@@ -0,0 +1,8 @@
+﻿using PlataformaPedidos.Dominio.Modelos;
+
+namespace PlataformaPedidos.Dominio.Contratos;
+
+public interface IServicioCliente
+{
+    public Cliente CrearCliente(string nombre);
+}
